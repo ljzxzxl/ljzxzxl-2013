8d7d9e5b98ce62f42c5f519f5adb9a637a835a81
ljzxzxl@gmail.com <ljzxzxl@gmail.com@8cc6263b-7ac8-ee61-ad12-06d5dcc9e3fd>
2013-01-11 08:44:49 +0000

    simple_html_dom测试用例

---
+++ simplehtmldom_1_5/test.php
@@ -0,0 +1,17 @@
+<?php
+include "simple_html_dom.php" ;
+// Create DOM from URL or file
+$html = file_get_html('http://www.baidu.com/');
+
+// Find all images 
+foreach($html->find('img') as $element) 
+       echo $element->src . '<br>';
+
+// Find all links 
+foreach($html->find('a') as $element) 
+       echo $element->href . '<br>';
+
+// Find title
+foreach($html->find('title') as $element) 
+       echo $element->title . '<br>';
+exit;
